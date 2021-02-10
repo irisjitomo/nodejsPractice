e169641ecb4744bf63fb07d0072d6dfa863fc5ff
unknown <irisjitomo@yahoo.com>
2021-02-10 12:29:40 -0500

    readline with events

--- readline.js
+++ readline.js
@@ -1,2 +1,35 @@
 const readline = require('readline');
-readline.createInterface()
+const rl = readline.createInterface({input : process.stdin,
+                                     output: process.stdout});
+
+let num1 = Math.floor((Math.random() * 10)) + 1;
+let num2 = Math.floor((Math.random() * 10)) + 1;
+
+let answer = num1 + num2
+
+rl.question(`What is ${num1} + ${num2}? \n`,
+(userInput) => {
+    if (userInput.trim() == answer) {
+        console.log('Correct')
+        rl.close()
+    }
+    else{
+        rl.setPrompt('Incorrect, try again :) \n', )
+        rl.prompt()
+        rl.on('line', userInput => {
+            if (userInput.trim() == answer) {
+                rl.close()
+            } else {
+                rl.setPrompt(`Your answer of ${userInput} is incorrect \n`)
+                rl.prompt()
+            }
+        })
+    }
+});
+
+// Here we listen for an event 'close'. Since readline is an 
+// instance of the eventEmitter class, we can use eventEmitter
+// methods
+rl.on('close', () => {
+    console.log('Game Over')
+});
